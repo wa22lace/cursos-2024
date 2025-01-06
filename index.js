59ebf8a488a0b479c4759b35d5757745ff0b18bb
wa22lace <48635988+wa22lace@users.noreply.github.com>
2025-01-06 00:04:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const butao = document.getElementById('teste')
+
+
+
+setTimeout(() =>{butao.click();},400000);
+
+
+var teste = document.getElementById('teste');
+teste.onclick = function(){
+         window.location = "https://www.profitablecpmrate.com/akv60cf7?key=68aa0b605468f42199fdd436db3f9186"
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
